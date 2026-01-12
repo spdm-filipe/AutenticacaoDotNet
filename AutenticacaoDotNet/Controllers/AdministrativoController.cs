dd867606058b3485b506f787354084f1805e3c2d
Filipe Augusto Santos Chaves <filipe.chaves@spdm-pais.org.br>
2026-01-12 14:28:33 -0300

    ajustes. incluindo nova autenticação

---
+++ AutenticacaoDotNet/Controllers/AdministrativoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutenticacaoDotNet.Controllers
+{
+    [Authorize(Policy = "Admin")]
+    public class AdministrativoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
